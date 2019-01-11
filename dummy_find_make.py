ddebb1e7e4c5284fd96edb67cb20ef41cc21bdf5
omegadan01 <dbruzzes@nd.edi>
2019-01-11 00:14:01 -0500

    added counting of incomplete fasta sequence

--- dummy_find_make.py
+++ dummy_find_make.py
@@ -1,4 +1,5 @@
 import re
+from Bio import SeqIO
 
 # load full COI file
 full = open("COI_dataset.fasta", "r")
@@ -6,10 +7,22 @@ full = open("COI_dataset.fasta", "r")
 # load partial H3 file
 h3 = open("6-gene_dataset_H3_aligned.fasta", "r")
 
+hits_dict = {}
+
+max_len = 0
+max_description = ""
+for record in SeqIO.parse("6-gene_dataset_H3_aligned.fasta", "fasta"):
+    if len(record) > max_len:
+        max_len = len(record)
+        max_description = record.description
+
+print(max_description)
+print(max_len)
+
+# remove header from mompha taxa (H3_ from H3 fasta)
 search_tex = r'H3_'
 replace_tex = r''
 
-# remove header from mompha taxa (H3_ from H3 fasta)
 for line in h3:
     line = line.strip()
     if ">" in line:
@@ -17,6 +30,12 @@ for line in h3:
         print(line)
     else:
         print(line)
+
+
+# string of Ns equal to max length
+string_val = "N" * max_len
+print string_val
+
 
 
 
